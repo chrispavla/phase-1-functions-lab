1241df0396a1000604eb7e4dc744d571d51f6f2a
Kristina Voroteliak <kristina.hodges7@gmail.com>
2022-07-18 12:37:00 -0400

    commit tree a3ec2f8f67860e86554347f87c91f85842bca268

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Code your solution in this file!
+// function distanceFromHqInBlocks(someValue) {
+// return Math.abs((someValue - 42));
+// }
+
+// function distanceFromHqInFeet(someValue) {
+// return distanceFromHqInBlocks(someValue) * 264;
+// }
+
+// function distanceTravelledInFeet(start, destination) {
+//     return Math.abs((start - destination) * 264);
+// }
+
+// function calculatesFarePrice(start, destination) {
+
+//     if (distanceTravelledInFeet(start, destination) <= 400)  {
+
+//         return 0;
+//     } else if(distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000) {
+
+//         return ((distanceTravelledInFeet(start, destination) - 400)*2) / 100;
+//     } else if(distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+
+//         return 25;
+//     } else if(distanceTravelledInFeet(start, destination) > 2500) {
+
+//         return 'cannot travel that far';
+//     }
+// }
+function distanceFromHqInBlocks(pickupLocation) {
+  return Math.abs(pickupLocation - 42);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+  const distance = distanceFromHqInBlocks(pickupLocation);
+  return distance * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distanceInFeet = distanceTravelledInFeet(start, destination);
+  if (distanceInFeet < 400) {
+    return 0;
+  } else if (distanceInFeet > 400 && distanceInFeet < 2000) {
+    return (distanceInFeet - 400) * 0.02;
+  } else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+    return 25;
+  } else if (distanceInFeet > 2500) {
+    return "cannot travel that far";
+  }
+}
